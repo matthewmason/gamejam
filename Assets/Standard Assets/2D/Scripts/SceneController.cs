ec6c5d50f6bde55b3e8804b70c3c5fc5174c9a64
cyril.nxumalo <cyril.nxumalo@kateryna-tertiienko.mobile.local>
2017-08-05 21:49:15 +0200

    adding global counter to script

---
+++ Assets/Standard Assets/2D/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+	public Text countText;
+	public Transform player;
+
+	private int count;
+
+	private void Awake()
+	{
+		count = 0;
+		SetCountText();
+	}
+
+	private void Update()
+	{
+		int newPos = (int)6.39 + (int)player.position.x;
+
+		if (count <= newPos) {
+			count = newPos;
+		}
+			
+		SetCountText();
+	}
+
+	private void SetCountText ()
+	{
+		countText.text = "Score: " + count.ToString ();
+	}
+}
