2bfd2b857b43df97b45800c5b0e05512189c06ec
cyril.nxumalo <cyril.nxumalo@kateryna-tertiienko.mobile.local>
2017-08-05 17:13:18 +0200

    commit changes with camera fix and background

---
+++ Assets/Standard Assets/2D/Scripts/Background2DFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background2DFollower : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = target.position;
+		newPos.y = 7.8f;
+		transform.position = newPos;
+	}
+}
--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -23,8 +23,7 @@ namespace UnityStandardAssets._2D
             m_OffsetZ = (transform.position - target.position).z;
             transform.parent = null;
         }
-
-
+			
         // Update is called once per frame
         private void Update()
         {
@@ -41,9 +40,11 @@ namespace UnityStandardAssets._2D
             {
                 m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
             }
-
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+				
+			Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
             Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+
+			newPos.y = 7.8f;
 
             transform.position = newPos;
 
