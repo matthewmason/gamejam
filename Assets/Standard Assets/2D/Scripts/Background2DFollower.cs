ea60eaad3c04611a3d7eb95ececed2cf063f3e6a
cyril.nxumalo <cyril.nxumalo@kateryna-tertiienko.mobile.local>
2017-08-05 18:31:39 +0200

    fixing overlapping background

--- Assets/Standard Assets/2D/Scripts/Background2DFollower.cs
+++ Assets/Standard Assets/2D/Scripts/Background2DFollower.cs
@@ -8,7 +8,7 @@ public class Background2DFollower : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform.parent = null;
+//		transform.parent = null;
 	}
 	
 	// Update is called once per frame
