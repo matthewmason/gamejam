6eea6c196e467bab642242916a49faee245c2171
cyril.nxumalo <cyril.nxumalo@MacBook-Pro-2.local>
2017-08-06 11:48:31 +0200

    ai and style to map

---
+++ Assets/Standard Assets/2D/Scripts/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	public Transform target;
+	public float moveSpeed;
+	public int rotationSpeed;
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Awake() {
+		myTransform = transform;
+	}
+
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+		target = go.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {    
+		Vector3 dir = target.position - myTransform.position;
+		dir.z = 0.0f; // Only needed if objects don't share 'z' value
+		if (dir != Vector3.zero) {
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, 
+				Quaternion.FromToRotation(Vector3.right, dir), rotationSpeed * Time.deltaTime);
+		}
+
+		//Move Towards Target
+		myTransform.position += myTransform.position * moveSpeed * Time.deltaTime;
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag == "Ground" )
+		{
+			
+		}
+	}
+}
